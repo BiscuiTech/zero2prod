7bec23a482ae4122bc6331147ba1eb04e1d7bfd0
BiscuiTech <jhuet@biscui.tech>
2023-09-10 18:06:50 -0400

    wip

---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,9 @@
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}

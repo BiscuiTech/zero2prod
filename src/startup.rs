dcb867110db2203c1a5d28fa45991112cc6cdae2
Jean-Cédric Huet <jhuet@biscui.tech>
2024-08-11 09:00:19 -0400

    up to 3.8.5.2.2

--- src/startup.rs
+++ src/startup.rs
@@ -2,17 +2,13 @@ use std::net::TcpListener;
 
 use actix_web::{dev::Server, HttpServer};
 
+use crate::routes::{health_check, subscribe};
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         actix_web::App::new()
-            .route(
-                "/health_check",
-                actix_web::web::get().to(crate::routes::health_check),
-            )
-            .route(
-                "/subscriptions",
-                actix_web::web::post().to(crate::routes::subscribe),
-            )
+            .route("/health_check", actix_web::web::get().to(health_check))
+            .route("/subscriptions", actix_web::web::post().to(subscribe))
     })
     .listen(listener)?
     .run();

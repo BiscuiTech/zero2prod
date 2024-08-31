d0ccc6efaca73ad244704cd780304764c441c4d7
Jean-Cédric Huet <jhuet@biscui.tech>
2024-08-31 10:06:22 -0400

    wip

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -9,11 +9,8 @@ pub struct FormData {
     name: String,
 }
 
-pub async fn subscribe(
-    form: web::Form<FormData>,
-    connection: web::Data<PgConnection>,
-) -> HttpResponse {
-    sqlx::query!(
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgConnection>) -> HttpResponse {
+    match sqlx::query!(
         r#"
             INSERT INTO subscriptions (id, email, name, subscribed_at)
             VALUES ($1, $2, $3, $4)   
@@ -23,7 +20,15 @@ pub async fn subscribe(
         form.name,
         Utc::now()
     )
-    .execute(connection.get_ref())
-    .await;
-    HttpResponse::Ok().finish()
+    // We use `get_ref` to get an immutable reference to the `PgConnection`
+    // wrapped by `web::Data`.
+    .execute(pool.get_ref())
+    .await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("Failed to execute query: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
--- src/startup.rs
+++ src/startup.rs
@@ -5,12 +5,12 @@ use sqlx::PgConnection;
 use std::net::TcpListener;
 
 pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
-    let connection = web::Data::new(connection);
+    let db_pool = web::Data::new(connection);
     let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
-            .app_data(connection.clone())
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();

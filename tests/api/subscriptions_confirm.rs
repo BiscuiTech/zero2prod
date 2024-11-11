24f9ddd2ea8b254af0a779f061f9a12687a18590
Jean-Cédric Huet <jhuet@biscui.tech>
2024-11-11 18:49:42 -0500

    8.0

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -30,7 +30,6 @@ async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
     app.post_subscriptions(body.into()).await;
     let email_request = &app.email_server.received_requests().await.unwrap()[0];
     let confirmation_links = app.get_confirmation_links(&email_request);
-
     let response = reqwest::get(confirmation_links.html).await.unwrap();
 
     assert_eq!(response.status().as_u16(), 200);
